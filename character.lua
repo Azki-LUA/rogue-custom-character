e745e31e42fa780bb1cf5977bbf83c8442f5512e
Azki-LUA <74989734+Azki-LUA@users.noreply.github.com>
2021-01-12 13:06:27 -0500

    Create character.lua

---
+++ character.lua
@@ -0,0 +1,20 @@
+-- WORK ON PROGRESS
+
+
+
+
+
+
+
+
+-- settings
+local leaderboardname = "Sigma"
+local charactername = "SIGMA" -- make it uppercase or it wont work
+local lives = "9"
+local leaderboardlastname = "" -- make blank if cameo
+-- script
+game.Players.LocalPlayer.leaderstats.FirstName.Value = leaderboardname
+game.Players.LocalPlayer.leaderstats.LastName.Value = ""
+game.Players.LocalPlayer.PlayerGui.StatGui.Container.CharacterName.Text = charactername
+game.Players.LocalPlayer.PlayerGui.StatGui.Container.CharacterName.Shadow.Text = charactername
+game.Players.LocalPlayer.PlayerGui.StatGui.Container.Health.Lives.Roller.Char.Text = lives
